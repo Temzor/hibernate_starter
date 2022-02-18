f4d61f05f70e5b559b0d5794b90909c82494b6fa
Капустин Дмитрий <onewaykd@gmail.com>
2022-02-18 20:44:30 +0300

    1. Введение; 4. Конфигурация SessionFactory.

--- src/main/java/com/hibernate/HibernateRunner.java
+++ src/main/java/com/hibernate/HibernateRunner.java
@@ -1,13 +1,18 @@
 package com.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import java.sql.SQLException;
 
 public class HibernateRunner {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         Configuration configuration = new Configuration();
         configuration.configure();
 
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            System.out.println("ok");
+        }
     }
 }
