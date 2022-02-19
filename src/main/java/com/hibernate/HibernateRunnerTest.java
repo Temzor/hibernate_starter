1c8cce827a1a0628e28844ba393f9f9dc677aa63
Капустин Дмитрий <onewaykd@gmail.com>
2022-02-19 17:52:32 +0300

    1. Введение; 6. Класс Session.

---
+++ src/main/java/com/hibernate/HibernateRunnerTest.java
@@ -0,0 +1,51 @@
+package com.hibernate;
+
+import com.hibernate.entity.User;
+import org.junit.jupiter.api.Test;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import static java.util.Optional.ofNullable;
+
+class HibernateRunnerTest {
+
+    @Test
+    void checkReflectionApi() {
+        User user = User.builder()
+                .username("Ter@mail.ru")
+                .firstname("Ivan")
+                .lastname("Ivanov")
+                .birthDate(LocalDate.of(2000, 1, 20))
+                .age(21)
+                .build();
+
+        String sql = """
+                insert
+                into
+                %s
+                (%s)
+                values
+                (%s)
+                """;
+        String tableName = ofNullable(user.getClass().getAnnotation(Table.class))
+                .map(tableAnnotation -> tableAnnotation.schema() + "." + tableAnnotation.name())
+                .orElse(user.getClass().getName());
+
+        Field[] declaredFields = user.getClass().getDeclaredFields();
+
+        String columnNames = Arrays.stream(declaredFields)
+                .map(field -> ofNullable(field.getAnnotation(Column.class))
+                        .map(Column::name)
+                        .orElse(field.getName()))
+                .collect(Collectors.joining(", "));
+
+        String columnValues = Arrays.stream(declaredFields)
+                .map(field -> "?")
+                .collect(Collectors.joining(", "));
+
+        System.out.printf((sql) + "%n", tableName, columnNames, columnValues);
+    }
+}
